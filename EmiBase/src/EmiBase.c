f0a963cb2a79820cb0d931d0592ca62f1f8a829c
EmK530 <50961259+EmK530@users.noreply.github.com>
2026-07-07 05:47:57 +0200

    Remove debug print

--- EmiBase/src/EmiBase.c
+++ EmiBase/src/EmiBase.c
@@ -73,7 +73,6 @@ void EmiBase_ProcessInput()
     int key;
     while ((key = GetKeyPressed()) != 0)
     {
-        printf("top = %i\n", scene_stack.top);
         for (int i = 0; i <= scene_stack.top; i++) {
             Scene *s = scene_stack.scenes[i];
             if (s && s->OnInput) {
